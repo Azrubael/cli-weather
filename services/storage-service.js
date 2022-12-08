8b00fd40f50c3d21316628fccd8d51547b58a05b
Azrubael <i0638464000@gmail.com>
2022-12-08 14:58:10 +0200

    Lesson #38. Added saving of the token

--- services/storage-service.js
+++ services/storage-service.js
@@ -1,13 +1,46 @@
 import { homedir } from 'os'
-import { join, basename, dirname, relative, isAbsolute } from 'path'  // кроссплатформенное разрешение путей
+// импортируем библиотеку для кроссплатформенного разрешения путей
+import { join, basename, dirname, relative, isAbsolute } from 'path'  
+import { promises } from 'fs'
 
 const filePath = join(homedir(), './weather-data.json')
 
-function saveKeyValue(key, value) {
+async function saveKeyValue(key, value) {
+   let data = {}
+   if (await isExist(filePath)) {
+      const file = await promises.readFile(filePath)
+      data = JSON.parse(file)
+   }
+   data[key] = value
+   await promises.writeFile(filePath, JSON.stringify(data))
+
+   /* примеры обработки путей к файлу методами 'path'
    console.log(basename(filePath))     // вывод последней части пути
    console.log(dirname(filePath))      // вывод последней части пути
    console.log(relative(filePath, dirname(filePath))) // относительный путь
    console.log(isAbsolute(filePath))
+   */
 }
 
-export { saveKeyValue } 
+// получение 'value' по 'key', если файл существует
+const getKeyValue = async (key) => {
+   if (await isExist(filePath)) {
+      const file = await promises.readFile(filePath)
+      let data = JSON.parse(file)
+      return data[key]   
+   }
+   return undefined
+}
+
+// проверка существования файла (если путь существует)
+const isExist = async (path) => {
+   try {
+      await promises.stat(path)
+      return true
+   } catch(e) {
+      return false
+   }
+   
+}
+
+export { saveKeyValue, getKeyValue } 
--- weather.js
+++ weather.js
@@ -4,10 +4,21 @@
 // модуля с использованием NodeJS:
 
 import { getArgs } from './helpers/args.js'
-import { printHelp, printSuccess } from './services/log-service.js'
-import { saveKeyValue } from './services/storage-service.js'
+import { printHelp, printSuccess, printError } from './services/log-service.js'
+import { saveKeyValue, getKeyValue } from './services/storage-service.js'
 
-const initCLI = () => {
+async function saveToken(token) {
+	try {
+		await saveKeyValue('token', token)
+		printSuccess('Токен сохранен')
+	} catch(e) {
+		printError(e.message)
+	}
+	
+}
+
+
+function initCLI() {
 	const args = getArgs(process.argv)
 
 	// листинг актуальных аргументов командной строки
@@ -23,11 +34,10 @@ const initCLI = () => {
 	}
 	if (args.t) {
 		// Сохранить токен
-		saveKeyValue('token', args.t)
+		return saveToken(args.t)
 	}
 	// Иначе вывести погоду с текущими настройками
-	let msg = ''
-	printSuccess(msg)
+		printSuccess('Вывод данных о погоде')
 }
 
 initCLI()
