8114e6fdd8862150ab3b7f0e2cd00db5be12f0fd
Azrubael <i0638464000@gmail.com>
2022-12-09 20:17:46 +0200

    Lesson #039. 'axios' was added

--- services/api-service.js
+++ services/api-service.js
@@ -1,4 +1,6 @@
-import https from 'https'
+// 'axios' более удобен, чем 'https', т.к. структурирует не только ввод, но и вывод
+import axios from 'axios'
+// import https from 'https'  // заменен 'axios'
 import { getKeyValue, TOKEN_DIC } from './storage-service.js'
 
 // метод получения погоды
@@ -7,22 +9,16 @@ async function getWeather(city) {
    if (!token) {
       throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]')
    }
-   // const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
-   const url = new URL('https://api.openweathermap.org/data/2.5/weather')
-   url.searchParams.append('q', city)
-   url.searchParams.append('appid', token)
-   url.searchParams.append('lang', 'ru')
-   url.searchParams.append('units', 'metric')
-
-   https.get(url, (response) => {
-      let result = ''
-      response.on('data', (chunk) => {
-         result += chunk
-      })
-      response.on('end', () => {
-         console.log(result)
-      })
+   const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+      params: {
+         q: city,
+         appid: token,
+         lang: 'ru',
+         units: 'metric'
+      }
    })
+   // console.log(data)    // структурированный вывод объекта 'data'
+   return data
 }
 
 export { getWeather }
